f801987c583e2cb71d7773d2ba2dd32056ea4645
Daniel <danila_varfolomeev@epam.com>
2020-05-24 10:44:51 +0300

    Added script for collecting hosts.

--- environment/dev/Helpers.ps1
+++ environment/dev/Helpers.ps1
@@ -22,3 +22,22 @@ function Set-EnvVariable {
     param([string]$Key, [string]$Value)
     [System.Environment]::SetEnvironmentVariable($Key, $Value, "User")
 }
+
+function Get-ComposeVariable {
+    param([string]$Key)
+    $userValue = [System.Environment]::GetEnvironmentVariable($Key, "User")
+    if ($null -ne $userValue) {
+        return $userValue
+    }
+    $machineValue = [System.Environment]::GetEnvironmentVariable($Key, "Machine")
+    if ($null -ne $machineValue) {
+        return $machineValue
+    }
+    $envLine = (Get-Content ".\.env" | Where-Object { $_.Contains("$Key=") } | Select-Object -First 1)
+    if ($null -ne $envLine) {
+        $envValue = ($envLine -split "=")[1]
+        return $envValue
+    }
+    Write-Host "Attempting to read undefined environment variable ""$Key""" -fore Yellow
+    return $null
+}
---
+++ environment/dev/Set-Hosts.ps1
@@ -0,0 +1,48 @@
+.(".\Helpers.ps1")
+
+function Update-HostEntry() {
+    param([string]$IP, [string]$HostName)
+
+    $hostsPath = "$($Env:WinDir)\system32\Drivers\etc\hosts"
+    $hostsFileContent = Get-Content $hostsPath
+    $escapedHostName = [Regex]::Escape($HostName)
+    
+    if (($hostsFileContent) -match ".*\s+$escapedHostName$") {
+        Write-Host ("Removing {0} record from hosts file." -f $HostName)
+        $hostsFileContent -notmatch ".*\s+$escapedHostName$" | Out-File $hostsPath
+        # Required to avoid write conflict
+        Start-Sleep -Milliseconds 300
+    }
+
+    if ($IP) {
+        Write-Host ("Adding {0} {1} record to hosts file." -f $IP, $HostName)
+        Add-Content -Encoding UTF8 $hostsPath ("{0} {1}" -f $IP, $HostName)
+        # Required to avoid write conflict
+        Start-Sleep -Milliseconds 300
+    }
+}
+
+function Get-Ip(){
+    param([string]$ContainerName)
+    return docker container inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $ContainerName
+}
+
+function Update-ContainerHosts(){
+    param([string]$ContainerName, [string]$ComposeVariable)
+ 
+    $ip = Get-Ip -ContainerName $ContainerName
+    $hosts = (Get-ComposeVariable $ComposeVariable) -split ";"
+    $hosts | ForEach-Object {
+        Update-HostEntry -IP $ip -HostName $_
+    }
+}
+
+$projectName = Get-ComposeVariable -Key "COMPOSE_PROJECT_NAME"
+
+Update-ContainerHosts -ContainerName "$($projectName)_cm" -ComposeVariable "HOSTS_CM"
+Update-ContainerHosts -ContainerName "$($projectName)_cd" -ComposeVariable "HOSTS_CD"
+Update-ContainerHosts -ContainerName "$($projectName)_identity" -ComposeVariable "HOSTS_IDENTITY"
+Update-ContainerHosts -ContainerName "$($projectName)_bizfx" -ComposeVariable "HOSTS_BIZFX"
+Update-ContainerHosts -ContainerName "$($projectName)_commerce-authoring" -ComposeVariable "HOSTS_COMMERCE-AUTHORING"
+
+Write-Host "Hosts file updated." -fore Green
